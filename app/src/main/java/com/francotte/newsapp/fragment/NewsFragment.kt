2a6ee3badbfaff5dd682063314b4e985bef5a24a
Rodolphe18 <rodolphefrancotte18@gmail.com>
2022-12-01 16:06:53 +0100

    first commit

---
+++ app/src/main/java/com/francotte/newsapp/fragment/NewsFragment.kt
@@ -0,0 +1,25 @@
+package com.francotte.newsapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.francotte.newsapp.R
+
+
+class NewsFragment : Fragment(R.layout.fragment_news) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+}
